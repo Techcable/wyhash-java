d2d38b5ee892a30e60e6104de237d06663403e0a
Techcable <techcable@techcable.net>
2023-08-09 23:20:47 -0700

    Use the git-version gradle plugin for versioning.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("wyhash.java-library-conventions")
+    id("com.palantir.git-version") version "3.0.0"
 }
+
+val gitVersion: groovy.lang.Closure<String> by extra
+version = gitVersion()
 
 java {
     withSourcesJar()
